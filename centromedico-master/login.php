712a31aa495ebc1192c0f402496c6932023a0dc7
JuanDrd435 <80801399+JuanDrd435@users.noreply.github.com>
2021-11-19 21:38:22 -0500

    Update login.php

--- centromedico-master/login.php
+++ centromedico-master/login.php
@@ -7,7 +7,7 @@ if (isset($_SESSION['usuario'])){
 if($_SERVER['REQUEST_METHOD']=='POST'){
 	$usuario = filter_var(strtolower($_POST['usuario']),FILTER_SANITIZE_STRING);
 	$password = $_POST['password'];
-	$password = hash('sha512', $password);
+	//$password = hash('sha512', $password);
 	$errores ='';	
 	try{
 		$conexion = new PDO('mysql:host=localhost;dbname=centromedico','root','');
