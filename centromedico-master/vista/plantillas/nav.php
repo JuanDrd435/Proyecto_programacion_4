d2cb84f568557661239a885aef776ce2bfa82caa
Hilary Paz <hila.p.30.18@gmail.com>
2021-11-27 13:14:16 -0500

    index normatividad

--- centromedico-master/vista/plantillas/nav.php
+++ centromedico-master/vista/plantillas/nav.php
@@ -1,14 +1,14 @@
 <aside>
 	<div class="widget">
 			<a href="index.php" title="CenterMedicine"><i class="fa fa-camera-retro fa-lg"></i> Centro Medico</a>
-			<a href="citas.php" title="Citas">Citas</a>
-			<a href="medicos.php" title="Medicos">Medicos</a>
-			<a href="pacientes.php" title="Pacientes">Pacientes</a>
-			<a href="consultorios.php" title="Consultorios">Consultorios</a>
-            <a href="especialidades.php" >Especialidades</a>
-			<a href="usuarios.php" title="Usuarios">Usuarios</a>
-			<a href="paisCiudad.php" title="GestionPais">Gestion Paises</a>
-			<a href="Ciudad.php" title="GestionCiudad">Gestion Ciudades</a>
-			<a href="#" title="Reportes">Reportes</a>
+			<a href="citas.php" title="Citas"><img src="../asset/dash.svg" class="icon" alt="icon" srcset="">Citas</a>
+			<a href="medicos.php" title="Medicos"><img src="../asset/medic.svg" class="icon" alt="" srcset="" style="width:25px;">Medicos</a>
+			<a href="pacientes.php" title="Pacientes"><img src="../asset/pacientes.svg" class="icon" alt="" srcset=""style="width:25px;">Pacientes</a>
+			<a href="consultorios.php" title="Consultorios"><img src="../asset/location.svg" class="icon" alt="" srcset="">Consultorios</a>
+            <a href="especialidades.php" ><img src="../asset/especialidad.svg" class="icon" alt="" srcset=""style="width:25px;">Especialidades</a>
+			<a href="usuarios.php" title="Usuarios"><img src="../asset/usuario.svg" class="icon" alt="" srcset="" style="width:25px;"> Usuarios</a>
+			<a href="paisCiudad.php" title="GestionPais"><img src="../asset/world.svg" class="icon" alt="icon" srcset="">Gestion Paises</a>
+			<a href="Ciudad.php" title="GestionCiudad"><img src="../asset/city.svg" class="icon" alt="" srcset="">Gestion Ciudades</a>
+			<a href="#" title="Reportes"><img src="../asset/reportes.svg" class="icon" alt="" srcset=""style="width:25px;">Reportes</a>
 	</div>
 </aside>
